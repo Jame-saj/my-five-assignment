850e9c530c7f6ff4d3b20dd5222ac589f72c1099
Jame-saj <sharminakterjame26@gmail.com>
2026-03-07 15:30:37 +0600

    priority done

--- script.js
+++ script.js
@@ -11,41 +11,50 @@ async function allButtons()
 }
 function displayIssue(issues) {
   const containerIssue = document.getElementById('issue-container');
-  containerIssue.innerHTML = ' ';
+  containerIssue.innerHTML = '';
   issues.forEach(issue => {
+
+    const priorityStatus =
+      issue.priority === 'high'
+        ? 'bg-red-100 text-red-600'
+        : issue.priority === 'medium'
+          ? 'bg-orange-100 text-orange-600'
+          : 'bg-gray-100 text-gray-500';
+
     const div = document.createElement('div');
     div.innerHTML = `
-          <div class="p-4 border rounded-lg shadow bg-white">
-        <div class='issue-check flex justify-between'>
-          <p> ${
-            issue.status === 'open'
-              ? `<img src="./assets/Open-Status.png" alt="" srcset="">`
-              : `<img
-              src="./assets/Closed- Status .png" alt="">`
-          }
-          </p>
-          <span>${issue.priority}</span>
-        </div>
-      
-        <h3 class="font-semibold mt-2 mb-2">${issue.title}</h3>
-      
-        <p class="text-sm line-clamp-2 text-gray-500 mb-3">
-          ${issue.description}
-        </p>
-<div>
-  <p>${issue.labels}</p>
-</div>
-<br>
-<hr class="text-gray-400">
-<br>
-<p>#${issue.id} by ${issue.author}</p>
-<p>${issue.createdAt}</p>
-     
-      </div>
+         <div class="p-4 border rounded-lg shadow bg-white h-full flex flex-col justify-between">
 
-      `;
+  <div class="issue-check flex justify-between items-center mb-2">
+    ${
+      issue.status === 'open'
+        ? `<img src="./assets/Open-Status.png" class="w-6">`
+        : `<img src="./assets/Closed- Status .png" class="w-6">`
+    }
+
+    <span class="px-4 py-2 rounded-full text-sm ${priorityStatus}">
+      ${issue.priority.toUpperCase()}
+    </span>
+  </div>
+
+  <h3 class="font-semibold mt-2 mb-2">${issue.title}</h3>
+
+  <p class="text-sm line-clamp-2 text-gray-500 mb-3">
+    ${issue.description}
+  </p>
+
+  <p>${issue.labels}</p>
+
+  <hr class="my-3 text-gray-300">
+
+  <p>#${issue.id} by ${issue.author}</p>
+  <p>${issue.createdAt}</p>
+
+</div>
+`;
+
     containerIssue.appendChild(div);
     
    });
 }
-allButtons()
+allButtons();
